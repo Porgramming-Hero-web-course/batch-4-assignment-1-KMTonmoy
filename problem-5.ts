6d3dc7d9f1d4bd2b30a736f5fa5d63d43fb3e104
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-11-06 23:06:53 +0600

    Five NO Problem Solved

---
+++ problem-5.ts
@@ -0,0 +1,12 @@
+{
+    // ==============================
+    function getProperty<T, K extends keyof T>(object: T, property: K): T[K] {
+        return object[property];
+    }
+
+    const person = { name: "Alice", age: 30 };
+    console.log(getProperty(person, "name"));
+
+    // ==============================
+
+}
